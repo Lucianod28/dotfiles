c9dc94c6618f0b730ac8fc274c4d04829fd31547
Lucianod28 <Lucianod28@gmail.com>
2019-12-02 13:20:21 +0100

    stopped using ohmyzsh except for plugins

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# Update plugins
+git submodule update --recursive --remote
+
+# Symlink config files
 ln -sv ~/dotfiles/.vim ~
-ln -sv ~/dotfiles/.zshrc ~
+ln -sv ~/dotfiles/zsh/.zshrc ~
