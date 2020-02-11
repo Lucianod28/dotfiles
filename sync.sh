9aeaef625040a9e28ea785165682c3f4ec7971bf
Lucianod28 <Lucianod28@gmail.com>
2020-02-10 18:06:05 -0800

    added vim-plug installation to synch.sh

--- sync.sh
+++ sync.sh
@@ -6,3 +6,9 @@ git submodule update --recursive --remote
 # Symlink config files
 ln -sv ~/dotfiles/.vim ~
 ln -sv ~/dotfiles/zsh/.zshrc ~
+
+# Install vim-plug plugin manager
+if [ ! -f ~/dotfiles/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/dotfiles/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
