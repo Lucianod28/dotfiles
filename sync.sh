4d7fa576aeeb9fc78bd03c0211afa3ace3025151
Lucianod28 <Lucianod28@gmail.com>
2020-02-10 18:38:27 -0800

    sync.sh now creates undodir

--- sync.sh
+++ sync.sh
@@ -8,7 +8,9 @@ ln -sv ~/dotfiles/.vim ~
 ln -sv ~/dotfiles/zsh/.zshrc ~
 
 # Install vim-plug plugin manager
-if [ ! -f ~/dotfiles/.vim/autoload/plug.vim ]; then
-    curl -fLo ~/dotfiles/.vim/autoload/plug.vim --create-dirs \
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+mkdir ~/.vim/undodir
