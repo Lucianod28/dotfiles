5bff0318603cdbb7dda3664630ec09b4df894d96
Lucianod28 <Lucianod28@gmail.com>
2020-02-10 18:54:13 -0800

    added plugin loading to sync.sh

--- sync.sh
+++ sync.sh
@@ -12,5 +12,6 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+vim +PlugInstall +qall
 
 mkdir ~/.vim/undodir
