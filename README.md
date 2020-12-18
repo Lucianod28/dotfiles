ddc88f192954696c080722feba8628d522cf63f9
Lucianod28 <Lucianod28@gmail.com>
2020-12-17 16:47:46 -0800

    better install script, git config

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # dotfiles
 This repo contains the configuration files (dotfiles) and plugins (as git submodules) I use for vim
-and zsh.
+and zsh on Linux and macOS.
 
 ## Installation
-Before installation, remove existing `~/.vim/` and `~/.zshrc` files, as otherwise the new versions
-of these files will not be linked. Then run:
+Run:
 ```sh
 cd ~
 git clone https://github.com/Lucianod28/dotfiles
-cd dotfiles
-./install.sh
+./dotfiles/install.sh
 ```
-If you have any machine-specific zsh configuration you can create a `.zsh/local.sh` file and add it
-there.
+If you have any machine-specific zsh configuration you can create a `local.sh` file inside the `zsh`
+directory and add it there.
--- install.sh
+++ install.sh
@@ -1,11 +1,113 @@
 #!/bin/sh
 
-# Update plugins
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      # Skip symlink if it already exists
+      local currentSrc="$(readlink $dst)"
+      if [ "$currentSrc" == "$src" ]
+      then
+        skip=true;
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+
+# cd to root of dotfiles repo and get path
+cd "$(dirname "$0")"
+DOTFILES_ROOT=$(pwd -P)
+
+# Update git submodules (plugins and vim color scheme)
 git submodule update --init --recursive
 
-# Symlink config files
-ln -sv $HOME/dotfiles/.vim $HOME
-ln -sv $HOME/dotfiles/.zsh/zshrc $HOME/.zshrc
+# Symlink dotfiles
+overwrite_all=false backup_all=false skip_all=false
+link_file "$DOTFILES_ROOT/vim" "$HOME/.vim"
+link_file "$DOTFILES_ROOT/zsh/zshrc" "$HOME/.zshrc"
 
-mkdir $HOME/.vim/undo
-mkdir $HOME/.vim/swap
+# Configure git
+# By running git config commands instead of using a .gitconfig file, we avoid sourcing a local git
+# configuration or asking for username and other info when installing.
+git config --global pull.rebase true
+git config --global rerere.enabled true
+git config --global rerere.autoupdate true
+
+success 'configured git'
