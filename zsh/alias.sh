9a9c821e780677f97c6200c42c0ca0112891f2c8
Luciano de la Iglesia <Lucianod28@gmail.com>
2024-05-22 11:42:42 -0700

    Update venv alias for uv

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -9,4 +9,4 @@ alias jl="jupyter lab"
 alias hg='history 1|grep '
 alias zshrc="$EDITOR $HOME/.zshrc"
 alias vimrc="$EDITOR $HOME/.vim/vimrc"
-alias va="source venv/bin/activate"
+alias va="source .venv/bin/activate"
